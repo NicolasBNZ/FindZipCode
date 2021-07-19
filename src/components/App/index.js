c4a10a4b19b945137e6099633a2d177c20f04120
NicolasBNZ <nicolas.benzadon@gmail.com>
2021-07-19 15:10:45 +0200

    hook state on enter zip code is working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.scss';
 
-// Composants
+// Composants import
 import Zip from '../Zip';
 import City from '../City';
 
 
 
 function App() {
+
+  // enter a zip code number
+  const [search, setSearch] = useState('');
+  console.log(search);
+  
   return (
     <div className='app'>
       <h1> Trouver une commune avec son code postal</h1>
-      <Zip />
+      <Zip search={search} setSearch={setSearch} />
       <City />
     </div>
   );
--- src/components/Zip/index.js
+++ src/components/Zip/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 
 import './zip.scss';
 
-const Zip = () => {
+const Zip = ({ search, setSearch }) => {
+    // https://reactjs.org/docs/hooks-reference.html#useref
+    // we begin with an empty case
+    const refInput = useRef(null);
+
     return (
         <div className="zipcode">
-            <label for="zipcode">Entrer un code postal:</label>
-            <input type="text" id="zipcode" name="zipcode"></input>
+            <label htmlFor="zipcode" className="enterZip">Entrer un code postal:</label>
+            <input
+                className="spaceZip"
+                ref={refInput}
+                type="text"
+                placeholder="Rechercher..."
+                id="zipcode"
+                name="zipcode"
+                value={search}
+                onChange={(event) => {
+                    setSearch(event.target.value);
+                }}
+            ></input>
         </div>
-
     );
+};
+
+
+
+Zip.propTypes = {
+    search: PropTypes.string.isRequired,
+    setSearch: PropTypes.func.isRequired,
 };
 
 export default Zip;
